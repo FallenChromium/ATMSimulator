fae6adf8806a3b4502c7025fd9f941167cf9f683
FallenChromium <fallenchromium@gmail.com>
2022-03-26 02:04:43 +0300

    Add "DB" read exception

--- resources.py
+++ resources.py
@@ -6,28 +6,38 @@ class IncorrectPINException(Exception):
     def __str__(self):
         return "Incorrect PIN. Valid PIN: 4 digits"
 
+
 class BankAccountNotFoundException(Exception):
 
     def __str__(self):
         return "Bank account cannot be found"
+
 
 class IncorrectAmountValueException(Exception):
 
     def __str__(self):
         return "Nope. You can't use THAT as money amount"
 
+
 class NotEnoughFundsException(Exception):
 
     def __str__(self):
-        #this is a Starcraft reference
+        # this is a Starcraft reference
         return "Not enough minerals"
+
 
 class CardIsLockedException(Exception):
 
     def __str__(self):
-        #this is a Starcraft reference
-        return "Sorry, your card is locked. Visit the nearest bank office for assistance!" 
-        
+        # this is a Starcraft reference
+        return "Sorry, your card is locked. Visit the nearest bank office for assistance!"
+
+
+class ReadError(Exception):
+    def __str__(self):
+        return "Database is incorrect."
+
+
 class BanknoteTypes(enum.IntEnum):
     BYN5 = 5
     BYN10 = 10
