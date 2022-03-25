c16ba8dc1e4231355d0f848ea4f43b739e61b33d
FallenChromium <fallenchromium@gmail.com>
2022-03-25 23:21:35 +0300

    Update BaseMenu class

--- interface/BaseMenu.py
+++ interface/BaseMenu.py
@@ -1,4 +1,5 @@
 import abc
+from typing import Callable
 from rich.console import Console
 from rich.prompt import Prompt
 from rich.table import Table
@@ -7,7 +8,10 @@ import types
 
 class BaseMenu(abc.ABC):
     # int -> button in the interface; function -> action called; str -> description of the action.
-    __actions: dict[str, tuple[types.FunctionType, str]]
+    __actions: dict[str, tuple[Callable, str]]
+
+    def __init__(self, actionlist: dict[str, tuple[Callable, str]]):
+        self.__actions = actionlist
 
     def action(self, input):
         choices = list(self.__actions.keys())
@@ -17,8 +21,8 @@ class BaseMenu(abc.ABC):
     def show(self):
         console = Console()
         table = Table(show_header=True, header_style="bold cyan")
-        table.add_column("Button", style="dim", width=12)
-        table.add_column("Action", style="dim", width=12)
+        table.add_column("Button", style="dim", width=5)
+        table.add_column("Action", style="dim", width=30)
         for key, value in self.__actions.items():
             table.add_row(str(key), value[1])
         console.print(table)
