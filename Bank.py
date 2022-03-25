178da886c0c59c30feee1b3859b50c9218e281f0
FallenChromium <fallenchromium@gmail.com>
2022-03-25 19:14:45 +0300

    Card refactoring

--- Bank.py
+++ Bank.py
@@ -1,19 +1,23 @@
 from resources import NotEnoughFundsException, CardIsLockedException, BankAccountNotFoundException
-from utilities import generateBankAccountId, generateCardNumber, generatePIN, validatePIN
+from utilities import generateBankAccountId, generateCardNumber, generatePIN, validateAmount, validatePIN
 
 
 class BankAccount:
     _balance: int = 0
     _owner: str = ""
+    _id: str
 
-    def __init__(self, owner):
-        self._balance: int = 0
+    def __init__(self, owner, balance: int = 0, id: str = generateBankAccountId()):
+        self._balance: int = balance
         self._owner: str = owner
+        self._id: str = id
 
     def replenish(self, amount):
+        validateAmount(amount)
         self._balance += amount
 
     def withdraw(self, amount):
+        validateAmount(amount)
         if self._balance - amount < 0:
             raise NotEnoughFundsException
         else:
@@ -26,10 +30,43 @@ class BankAccount:
         return self._owner
 
 
+class Card:
+    _emitent: str
+    # card number and pin are strings because they can start with 0
+    _cardNumber: str
+    __bankAccountId: str
+    __pin: str
+    __incorrectTries: int = 0
+    _blocked: bool
+
+    def __init__(self, bankname: str):
+        # A card emitting operation. Should of course be more complicated, but it's a model after all
+        self._emitent = bankname
+        # generate a random number
+        self._cardNumber = generateCardNumber()
+        # generate a random PIN
+        self.__pin = generatePIN()
+        # Generate a random bank account ID
+        self.__bankAccountId = generateBankAccountId()
+
+    def getBankAccountId(self):
+        return self.__bankAccountId
+
+    def unlockCard(self, pin: str):
+        if self.__incorrectTries > 3:
+            raise CardIsLockedException
+        if self.__pin == pin:
+            return True
+        else:
+            self.__incorrectTries += 1
+            return False
+
+
 class Bank:
     _name: str
     # Uses card number as key
     _accounts: dict[str, BankAccount]
+    _cards: dict[str, Card]
 
     def __init__(self, name, accounts: dict[str, BankAccount]):
         self._name = name
@@ -53,35 +90,3 @@ class Bank:
 
     def withdrawFromAccount(self, account_id: str, amount: int):
         self._accounts[account_id].withdraw(amount)
-
-
-class Card:
-    _emitent: str
-    # card number and pin are strings because they can start with 0
-    _cardNumber: str
-    __bankAccountId: str
-    __pin: str
-    __incorrectTries: int = 0
-    _blocked: bool
-
-    def __init__(self, bank: Bank):
-        # A card emitting operation. Should of course be more complicated, but it's a model after all
-        self._emitent = bank.getName()
-        # generate a random number
-        self._cardNumber = generateCardNumber()
-        # generate a random PIN
-        self.__pin = generatePIN()
-        #Generate a random bank account ID
-        self.__bankAccountId = generateBankAccountId()
-
-    def getBankAccountId(self):
-        return self.__bankAccountId
-
-    def unlockCard(self, pin: str):
-        if self.__incorrectTries > 3:
-            raise CardIsLockedException
-        if self.__pin == pin:
-            return True
-        else:
-            self.__incorrectTries += 1
-            return False
